88f40fa0849d15d565ae1cd103d1e6d183a8d2e1
Stanley Wintergreen <kepocnhh@gmail.com>
2024-06-16 10:28:46 +0300

    TLS sample

--- sample/src/main/kotlin/sp/service/sample/App.kt
+++ sample/src/main/kotlin/sp/service/sample/App.kt
@@ -3,128 +3,13 @@ package sp.service.sample
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import sp.kx.http.HttpReceiver
-import sp.kx.http.HttpRequest
-import sp.kx.http.HttpResponse
-import sp.kx.http.HttpRouting
 import sp.service.sample.provider.FinalLoggers
 import sp.service.sample.provider.FinalSecrets
 import sp.service.sample.provider.Loggers
 import sp.service.sample.provider.Secrets
-
-private class AppRouting(
-    private val coroutineScope: CoroutineScope,
-    private val version: Int,
-    loggers: Loggers,
-    private val secrets: Secrets,
-) : HttpRouting {
-    sealed interface Event {
-        data object Quit : Event
-    }
-
-    private val logger = loggers.create("[Routing]")
-    private val _events = MutableSharedFlow<Event>()
-    val events = _events.asSharedFlow()
-
-    private val mapping = mapOf(
-        "/version" to mapOf(
-            "GET" to ::onGetVersion,
-        ),
-        "/quit" to mapOf(
-            "GET" to ::onGetQuit,
-        ),
-        "/session/start" to mapOf(
-            "POST" to ::onPostSessionStart,
-        ),
-    )
-
-    private fun onPostSessionStart(request: HttpRequest): HttpResponse {
-        logger.debug("on post session start...")
-        val body = request.body ?: return HttpResponse(
-            version = "1.1",
-            code = 500,
-            message = "Internal Server Error",
-            headers = mapOf(
-                "message" to "No body!",
-            ),
-            body = "todo".toByteArray(),
-        )
-        logger.debug("body(${body.size}): ${secrets.hash(body)}")
-        val publicKey = secrets.toPublicKey(body)
-        logger.debug("public:key: ${secrets.hash(publicKey.encoded)}")
-        return HttpResponse(
-            version = "1.1",
-            code = 500,
-            message = "Internal Server Error",
-            headers = emptyMap(),
-            body = "todo".toByteArray(),
-        )
-    }
-
-    private fun onGetVersion(request: HttpRequest): HttpResponse {
-        return HttpResponse(
-            version = "1.1",
-            code = 200,
-            message = "OK",
-            headers = emptyMap(),
-            body = "$version".toByteArray(),
-        )
-    }
-
-    private fun onGetQuit(request: HttpRequest): HttpResponse {
-        coroutineScope.launch {
-            _events.emit(Event.Quit)
-        }
-        return HttpResponse(
-            version = "1.1",
-            code = 200,
-            message = "OK",
-            headers = emptyMap(),
-            body = null,
-        )
-    }
-
-    override fun route(request: HttpRequest): HttpResponse {
-        logger.debug(
-            message = """
-                <-- request
-                ${request.method} ${request.query}
-                headers: ${request.headers}
-            """.trimIndent(),
-        )
-        val response = when (val route = mapping[request.query]) {
-            null -> HttpResponse(
-                version = "1.1",
-                code = 404,
-                message = "Not Found",
-                headers = emptyMap(),
-                body = null,
-            )
-            else -> when (val transform = route[request.method]) {
-                null -> HttpResponse(
-                    version = "1.1",
-                    code = 405,
-                    message = "Method Not Allowed",
-                    headers = emptyMap(),
-                    body = null,
-                )
-                else -> transform(request)
-            }
-        }
-        logger.debug(
-            message = """
-                --> response
-                ${response.code} ${response.message}
-                headers: ${response.headers}
-            """.trimIndent(),
-        )
-        return response
-    }
-}
 
 fun main() {
     val loggers: Loggers = FinalLoggers()
@@ -135,7 +20,6 @@ fun main() {
         val coroutineScope = CoroutineScope(Dispatchers.Default + job)
         coroutineScope.launch {
             val routing = AppRouting(
-                coroutineScope = this,
                 version = 2,
                 loggers = loggers,
                 secrets = secrets,
--- sample/src/main/kotlin/sp/service/sample/provider/FinalSecrets.kt
+++ sample/src/main/kotlin/sp/service/sample/provider/FinalSecrets.kt
@@ -4,6 +4,9 @@ import java.security.KeyFactory
 import java.security.MessageDigest
 import java.security.PublicKey
 import java.security.spec.X509EncodedKeySpec
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
 
 internal class FinalSecrets : Secrets {
     override fun toPublicKey(encoded: ByteArray): PublicKey {
@@ -15,5 +18,28 @@ internal class FinalSecrets : Secrets {
     override fun hash(bytes: ByteArray): String {
         val md = MessageDigest.getInstance("SHA256")
         return md.digest(bytes).joinToString(separator = "") { String.format("%02x", it.toInt() and 0xff) }
+    }
+
+    override fun newSecretKey(): SecretKey {
+        val generator = KeyGenerator.getInstance("AES")
+        return generator.generateKey()
+    }
+
+    override fun encrypt(publicKey: PublicKey, decrypted: ByteArray): ByteArray {
+        val cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey)
+        return cipher.doFinal(decrypted)
+    }
+
+    override fun encrypt(secretKey: SecretKey, decrypted: ByteArray): ByteArray {
+        val cipher = Cipher.getInstance("AES")
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+        return cipher.doFinal(decrypted)
+    }
+
+    override fun decrypt(secretKey: SecretKey, encrypted: ByteArray): ByteArray {
+        val cipher = Cipher.getInstance("AES")
+        cipher.init(Cipher.DECRYPT_MODE, secretKey)
+        return cipher.doFinal(encrypted)
     }
 }
